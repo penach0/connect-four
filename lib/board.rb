74e9ac0743eb50466af804dc058740c9beac73ae
penach0 <josemanuel.penacho@gmail.com>
2022-10-16 11:48:20 +0100

    Create Board#column
    - This method returns an array of a column from the board. This will
    be useful since the game is played from the top down
    - The board array is now initialized with elements composed of two
    white spaces for formatting reasons
    - Board now takes a board argument in its initialization in order to
    facilitate tests. I believe this is called dependency injection but I
    have to investigate the subject further

--- lib/board.rb
+++ lib/board.rb
@@ -1,7 +1,12 @@
 class Board
   attr_reader :board
 
-  def initialize
-    @board = Array.new(6) { Array.new(7, ' ') }
+  def initialize(board = nil)
+    @board = board || Array.new(6) { Array.new(7, '  ') }
+  end
+
+  def column(number)
+    columns = board.transpose
+    columns[number - 1]
   end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -2,15 +2,44 @@ require_relative '../lib/board'
 
 describe Board do
   subject(:board) { described_class.new }
-  it 'has a board attribute' do
-    expect(board).to have_attributes(board: Array)
+
+  describe '#initialize' do
+    context 'when board is initialized' do
+      it 'has a board attribute' do
+        expect(board).to have_attributes(board: Array)
+      end
+
+      it 'has 6 rows' do
+        expect(board.board.size).to eq(6)
+      end
+
+      it 'has 7 columns' do
+        expect(board.board[0].size).to eq(7)
+      end
+    end
   end
 
-  it 'has 6 rows' do
-    expect(board.board.size).to eq(6)
-  end
+  describe '#column' do
+    context 'when a column is empty' do
+      it 'returns the column' do
+        column = ['  '] * 6
+        expect(board.column(3)).to eq(column)
+      end
+    end
+    context 'when a column has pieces' do
+      subject(:played_board) do
+        described_class.new([['  ', '  ', '  ', '  ', '  ', '  '],
+                             ['  ', '  ', '  ', '  ', '  ', '  '],
+                             ['  ', '  ', '  ', '  ', '  ', '  '],
+                             ['  ', '  ', '  ', '  ', '  ', '  '],
+                             ['  ', '⚪', '  ', '  ', '  ', '  '],
+                             ['  ', '⚫', '  ', '  ', '  ', '  ']])
+      end
 
-  it 'has 7 files' do
-    expect(board.board[0].size).to eq(7)
+      it 'returns the column' do
+        column = ['  ', '  ', '  ', '  ', '⚪', '⚫']
+        expect(played_board.column(2)).to eq(column)
+      end
+    end
   end
 end
