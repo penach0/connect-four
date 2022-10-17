e074523e4cb57eecf963a2c82d172ea37029d55e
penach0 <josemanuel.penacho@gmail.com>
2022-10-17 08:38:25 +0100

    Create Game#pick_color
    - This method prompts the user about what color he wants to play.
    - White will always play first
    - Player objects now have a type: human or computer
    - The game will initially only support playing vs computer for
    simplicity
    - Game#create_players has been refactored to accommodate this changes

--- lib/game.rb
+++ lib/game.rb
@@ -1,8 +1,26 @@
 require_relative 'player'
 
+# Represents a single game of Connect 4
 class Game
-  def create_players
-    player1 = Player.new('⚪')
-    player2 = Player.new('⚫')
+  def create_players(color)
+    other_color = (color == '⚪' ? '⚫' : '⚪')
+    if color == '⚪'
+      @player1 = Player.new(color, 'human')
+      @player2 = Player.new(other_color, 'computer')
+    else
+      @player1 = Player.new(other_color, 'computer')
+      @player2 = Player.new(color, 'human')
+    end
+  end
+
+  def pick_color
+    print 'Play as White or Black (w/b): '
+    loop do
+      answer = gets.chomp.downcase
+      break if %w[w b].include?(answer)
+
+      print 'Please pick a valid option: '
+    end
+    answer == 'w' ? '⚪' : '⚫'
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -4,13 +4,13 @@ require_relative '../lib/player'
 describe Game do
   describe '#create_players' do
     subject(:game_players) { described_class.new }
-    let(:player1) { instance_double('Player', piece: '⚪') }
-    let(:player2) { instance_double('Player', piece: '⚫') }
+    let(:player1) { instance_double('Player', piece: '⚪', type: 'human') }
+    let(:player2) { instance_double('Player', piece: '⚫', type: 'computer') }
 
     it 'creates players' do
       player = class_double('Player').as_stubbed_const
       expect(player).to receive(:new).twice
-      game_players.create_players
+      game_players.create_players('⚪')
     end
   end
 end
