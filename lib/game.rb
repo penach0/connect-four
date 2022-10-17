867a15f44ef8b53e4ea1a6ee0748000ccbc832bc
penach0 <josemanuel.penacho@gmail.com>
2022-10-17 16:40:54 +0100

    Create Game#pick_column
    - This method prompts the user for a column to play sends the message
    to play it if valid
    - Unsure of the best place to ensure the validity of the choice,
    currently done by Board#playable_slot but may be moved in the future

--- lib/game.rb
+++ lib/game.rb
@@ -9,6 +9,11 @@ class Game
     @board = Board.new
   end
 
+  def setup
+    create_players(pick_color)
+    print_board
+  end
+
   def create_players(color)
     other_color = (color == '⚪' ? '⚫' : '⚪')
     if color == '⚪'
@@ -29,6 +34,18 @@ class Game
       print 'Please pick a valid option: '
     end
     answer == 'w' ? '⚪' : '⚫'
+  end
+
+  def pick_column(current_player)
+    print "#{current_player.piece} playing. Pick a number: "
+    loop do
+      column_number = gets.chomp.to_i
+
+      break current_player.make_play(board, column_number) if board.playable_slot(column_number)
+
+      print 'Not valid, pick another one: '
+    end
+
   end
 
   def print_board
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -4,8 +4,6 @@ require_relative '../lib/player'
 describe Game do
   describe '#create_players' do
     subject(:game_players) { described_class.new }
-    let(:player1) { instance_double('Player', piece: '⚪', type: 'human') }
-    let(:player2) { instance_double('Player', piece: '⚫', type: 'computer') }
 
     it 'creates players' do
       player = class_double('Player').as_stubbed_const
@@ -16,6 +14,7 @@ describe Game do
 
   describe '#print_board' do
     subject(:game_print) { described_class.new }
+
     it 'prints the board on the screen' do
       board = ['   ➊  ➋  ➌  ➍  ➎  ➏  ➐',
                '  ┃  |  |  |  |  |  |  ┃',
@@ -31,6 +30,22 @@ describe Game do
       end
 
       game_print.print_board
+    end
+  end
+
+  describe '#pick_column' do
+    subject(:game_pick) { described_class.new }
+    context 'when picking an available column' do
+      before do
+        allow(game_pick).to receive(:gets).and_return('1')
+      end
+      it 'sends message to Player to make play' do
+        current_player = instance_double('Player', piece: '⚫')
+        board = instance_double('Board')
+        column_number = 1
+        expect(current_player).to receive(:make_play).with(board, column_number)
+        game_pick.pick_column(current_player)
+      end
     end
   end
 end
