580cdbe5dadecbda7b36a3cbd7c00eaa02025159
penach0 <josemanuel.penacho@gmail.com>
2022-10-17 15:04:10 +0100

    Create Game#setup

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -3,20 +3,35 @@ require_relative '../lib/player'
 describe Player do
   subject(:player) { described_class.new('⚫') }
 
-  it 'has a color attribute' do
-    expect(player).to have_attributes(piece: String)
-  end
+  describe '#initialize' do
+    it 'has a color attribute' do
+      expect(player).to have_attributes(piece: String)
+    end
 
-  context 'when passed ⚫ as an argument' do
-    it 'sets ⚫ as piece' do
-      expect(player.instance_variable_get('@piece')).to eq('⚫')
+    context 'when passed ⚫ as an argument' do
+      it 'sets ⚫ as piece' do
+        expect(player.instance_variable_get('@piece')).to eq('⚫')
+      end
+    end
+
+    context 'when passed ⚪ as an argument' do
+      let(:player_white) { described_class.new('⚪') }
+      it 'sets ⚪ as piece' do
+        expect(player_white.instance_variable_get('@piece')).to eq('⚪')
+      end
     end
   end
 
-  context 'when passed ⚪ as an argument' do
-    let(:player_white) { described_class.new('⚪') }
-    it 'sets ⚪ as piece' do
-      expect(player_white.instance_variable_get('@piece')).to eq('⚪')
+  describe '#make_play' do
+    context 'when given a number' do
+      subject(:board) { instance_double('Board') }
+
+      it 'drops a piece in the corresponding column' do
+        piece = player.piece
+        column_number = 2
+        expect(board).to receive(:update_slot).with(column_number, piece)
+        player.make_play(board, column_number)
+      end
     end
   end
 end
