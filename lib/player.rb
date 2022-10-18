876f651f84b221a37d71492caa3e5f6c273f6f9d
penach0 <josemanuel.penacho@gmail.com>
2022-10-18 15:11:49 +0100

    Move Game#pick_column to Player class
    - Picking a column feels like a responsibility of a Player and not the
    game

--- lib/player.rb
+++ lib/player.rb
@@ -1,7 +1,23 @@
 class Player
   attr_reader :piece
 
-  def initialize(piece)
+  def initialize(piece, type)
     @piece = piece
+    @type = type
+  end
+
+  def make_play(board, column)
+    board.update_slot(column, piece)
+  end
+
+  def pick_column(board)
+    print "#{piece} playing. Pick a number: "
+    loop do
+      column_number = gets.chomp.to_i
+
+      return column_number if board.valid_play?(column_number)
+
+      print 'Not valid, pick another one: '
+    end
   end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,7 +1,8 @@
 require_relative '../lib/player'
+require_relative '../lib/board'
 
 describe Player do
-  subject(:player) { described_class.new('⚫') }
+  subject(:player) { described_class.new('⚫', 'human') }
 
   describe '#initialize' do
     it 'has a color attribute' do
@@ -15,7 +16,7 @@ describe Player do
     end
 
     context 'when passed ⚪ as an argument' do
-      let(:player_white) { described_class.new('⚪') }
+      let(:player_white) { described_class.new('⚪', 'human') }
       it 'sets ⚪ as piece' do
         expect(player_white.instance_variable_get('@piece')).to eq('⚪')
       end
@@ -26,11 +27,38 @@ describe Player do
     context 'when given a number' do
       subject(:board) { instance_double('Board') }
 
-      it 'drops a piece in the corresponding column' do
+      it 'sends message to update the respective slot' do
         piece = player.piece
         column_number = 2
         expect(board).to receive(:update_slot).with(column_number, piece)
         player.make_play(board, column_number)
+      end
+    end
+  end
+
+  describe '#pick_column' do
+    let(:pick_board) { instance_double('Board') }
+    context 'when picking an available column' do
+      before do
+        allow(player).to receive(:gets).and_return('1')
+        allow(pick_board).to receive(:valid_play?).and_return(true)
+      end
+      it 'returns the column number' do
+        column_number = 1
+        expect(player.pick_column(pick_board)).to eq(column_number)
+      end
+    end
+
+    context 'when picking invalid column once' do
+      before do
+        initial_message = '⚫ playing. Pick a number: '
+        allow(player).to receive(:print).with(initial_message)
+        allow(pick_board).to receive(:valid_play?).and_return(false, true)
+      end
+      it 'runs the loop twice' do
+        warning_message = 'Not valid, pick another one: '
+        expect(player).to receive(:print).with(warning_message)
+        player.pick_column(pick_board)
       end
     end
   end
