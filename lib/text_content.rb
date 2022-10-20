7136a59cca125bd0c2fed6c60832187816912717
penach0 <josemanuel.penacho@gmail.com>
2022-10-20 18:38:24 +0100

    Create TextContent Module
    - This module will hold text necessary for displaying messages
    - First thing added was an introductory screen

---
+++ lib/text_content.rb
@@ -0,0 +1,16 @@
+# This module will hold text content for message display
+module TextContent
+  INTRODUCTION = <<~HEREDOC.freeze
+
+
+  .g8"""bgd                                                  mm
+  .dP'     `M                                                  MM
+  dM'       ` ,pW"Wq.`7MMpMMMb.  `7MMpMMMb.  .gP"Ya   ,p6"bo mmMMmm          ,AM
+  MM         6W'   `Wb MM    MM    MM    MM ,M'   Yb 6M'  OO   MM           AVMM
+  MM.        8M     M8 MM    MM    MM    MM 8M"""""" 8M        MM         ,W' MM
+  `Mb.     ,'YA.   ,A9 MM    MM    MM    MM YM.    , YM.    ,  MM       ,W'   MM
+    `"bmmmd'  `Ybmd9'.JMML  JMML..JMML  JMML.`Mbmmd'  YMbmd'   `Mbmo    AmmmmmMMmm
+                                                                              MM
+                                                                              MM
+  HEREDOC
+end
